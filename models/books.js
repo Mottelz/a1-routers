20aac464b3147ac5ab0fc72ff197e95605fe3d66
Mottelz <shachna@gmail.com>
2025-03-13 13:37:06 -0400

    added error catching for db

--- models/books.js
+++ models/books.js
@@ -2,17 +2,32 @@ const { db } = require('./db_base');
 
 const getAllBooks = async () => {
     const stmnt = db.prepare("SELECT * FROM books;");
-    return await stmnt.all();
+    try {
+        return await stmnt.all();
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
 };
 
 const getBookById = async (id) => {
     const stmnt = db.prepare("SELECT * FROM books WHERE id=?;");
-    return await stmnt.get(id);
+    try {
+        return await stmnt.get(id);
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
 };
 
 const addBook = async ({author, title, isbn}) => {
     const stmnt = db.prepare("INSERT INTO books (author, title, isbn) VALUES (@author, @title, @isbn);");
-    return await stmnt.run({author, title, isbn});
+    try {
+        return await stmnt.run({author, title, isbn});
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
 };
 
 module.exports = {
