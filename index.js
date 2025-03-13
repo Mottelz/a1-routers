21d014270333cbad40fcf7d10e2906e18c363253
Mottelz <shachna@gmail.com>
2025-03-13 13:03:21 -0400

    removed mention of index

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 // Router Imports
-const gamesRouter = require("./routers/games.js");
 const mainRouter = require("./routers/main.js");
 const port = process.env.PORT || 3000;
 
@@ -15,7 +14,6 @@ app.set("view engine", "ejs");
 
 // Load routers
 app.use("", mainRouter);
-app.use("", gamesRouter);
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
