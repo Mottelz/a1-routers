4d802039d058156b375eb97543a20dbb8e70ac2c
Mottelz <shachna@gmail.com>
2025-03-13 12:59:28 -0400

    created assignment

---
+++ models/books.js
@@ -0,0 +1,22 @@
+const { db } = require('./db_base');
+
+const getAllBooks = async () => {
+    const stmnt = db.prepare("SELECT * FROM books;");
+    return await stmnt.all();
+};
+
+const getBookById = async (id) => {
+    const stmnt = db.prepare("SELECT * FROM books WHERE id=?;");
+    return await stmnt.get(id);
+};
+
+const addBook = async ({author, title, isbn}) => {
+    const stmnt = db.prepare("INSERT INTO books VALUES (@author, @title, @isbn);");
+    return await stmnt.run({author, title, isbn});
+};
+
+module.exports = {
+    getAllBooks,
+    getBookById,
+    addBook
+}
